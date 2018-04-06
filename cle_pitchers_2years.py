1840eddabf3233ffc67f7e00e028ae9d532a236c
Justin Long <33242991+justin-long@users.noreply.github.com>
2018-04-06 00:20:58 -0400

    updated to reset index

--- cle_pitchers_2years.py
+++ cle_pitchers_2years.py
@@ -7,13 +7,14 @@ Created on Wed Mar 28 18:31:33 2018
 
 import pandas as pd
 import numpy as np
-import mysql.connector
 
-one_game = pd.read_csv('C:/users/jblon/documents/datasets/one_game_for_char.csv')
-one_game
-list(one_game)
+events = pd.read_csv('C:/users/jblon/documents/Pitcher-Rotations/cle_2015_2016.csv')
+events
+list(events)
 
-few_col = one_game[['PIT_ID','PA_NEW_FL','PITCH_SEQ_TX', 'EVENT_TX', 'BATTEDBALL_CD']].copy()
+few_col = events[['PIT_ID','YEAR_ID','PA_NEW_FL','PITCH_SEQ_TX', 'EVENT_TX', 'BATTEDBALL_CD']].copy()
+
+few_col.head()
 
 def batted_count(colname, event):
     few_col[colname] = np.where(few_col['BATTEDBALL_CD'] == event, 1, 0)
@@ -36,12 +37,16 @@ few_col['BIP'] = np.where((few_col['BATTEDBALL_CD'] == 'G') |
                           (few_col['BATTEDBALL_CD'] == 'F') | 
                           (few_col['BATTEDBALL_CD'] == 'L'), 1, 0)
 
-grouped = few_col.groupby('PIT_ID').sum()
+grouped = few_col.groupby(['YEAR_ID','PIT_ID']).sum()
 
 grouped['GB_PER'] = grouped['GB_SUM'] / grouped['BIP']
 grouped['FB_PER'] = grouped['FB_SUM'] / grouped['BIP']
 grouped['LD_PER'] = grouped['LD_SUM'] / grouped['BIP']
 
+grouped.head()
+
+
+grouped = grouped.reset_index(level = ['YEAR_ID', 'PIT_ID'])
 
 grouped['SIERA'] = 6-17*(grouped['BB_SUM']/grouped['PA_CT'])
 
