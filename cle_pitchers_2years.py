3b3ea76c93446f3cb44438470aea838508ba499b
Justin Long <33242991+justin-long@users.noreply.github.com>
2018-04-15 19:05:57 -0400

    Updated code

--- cle_pitchers_2years.py
+++ cle_pitchers_2years.py
@@ -12,7 +12,7 @@ events = pd.read_csv('C:/users/jblon/documents/Pitcher-Rotations/cle_2015_2016.c
 events
 list(events)
 
-few_col = events[['PIT_ID','YEAR_ID','PA_NEW_FL','PITCH_SEQ_TX', 'EVENT_TX', 'BATTEDBALL_CD']].copy()
+few_col = events[['PIT_ID','YEAR_ID','PA_NEW_FL','PITCH_SEQ_TX', 'EVENT_TX', 'EVENT_CD', 'BATTEDBALL_CD', 'PA_NEW_FL']].copy()
 
 few_col.head()
 
@@ -27,10 +27,14 @@ few_col = few_col.join(temp)
 #Number of plate appearances
 few_col['PA_CT'] = np.where(few_col['PA_NEW_FL'] == 'T', 1, 0)
 
+#Number of Strike Outs
+few_col['SO_CT'] = np.where(few_col['EVENT_CD'] == 3, 1, 0)
+
 #calculate different batted ball counts
 batted_count('GB_SUM', 'G') 
 batted_count('FB_SUM', 'F')
 batted_count('LD_SUM', 'L')
+batted_count('PU_SUM', 'P')
 
 #calculate number of balls in play
 few_col['BIP'] = np.where((few_col['BATTEDBALL_CD'] == 'G') | 
@@ -42,16 +46,14 @@ grouped = few_col.groupby(['YEAR_ID','PIT_ID']).sum()
 grouped['GB_PER'] = grouped['GB_SUM'] / grouped['BIP']
 grouped['FB_PER'] = grouped['FB_SUM'] / grouped['BIP']
 grouped['LD_PER'] = grouped['LD_SUM'] / grouped['BIP']
+grouped['PU_PER'] = grouped['PU_SUM'] / grouped['BIP']
 
 grouped.head()
 
 
 grouped = grouped.reset_index(level = ['YEAR_ID', 'PIT_ID'])
 
-grouped['SIERA'] = 6-17*(grouped['BB_SUM']/grouped['PA_CT'])
-
+grouped['SIERA'] = 6.145 - 16.986*
+                
 grouped
 
-
-
-few_col
